01514bfb475ee48052e3523c8664f076450a9ae1
Shailesh Gokhale <68338746+shailesh-ss-19-11@users.noreply.github.com>
2025-10-13 20:13:21 +0530

    delete and useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
--- src/todolist/Todo.jsx
+++ src/todolist/Todo.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { MdDelete, MdEditSquare } from "react-icons/md";
 
 const Todo = () => {
+    console.log("inside component");
     const [input, setinput] = useState("");
     const [todoList, settodoList] = useState([]);
+
     const handleInputChange = (e) => {
         setinput(e.target.value);
     }
@@ -10,10 +13,11 @@ const Todo = () => {
     const addTodo = () => {
         if (input !== "") {
             let obj = {
-                taskname: input,
+                taskName: input,
                 id: new Date().getTime()
             }
             const newTodoList = [...todoList, obj];
+
             settodoList(newTodoList);
             setinput("");
         } else {
@@ -23,24 +27,86 @@ const Todo = () => {
 
     console.log(todoList)
 
+    const handleInputKeyDown = (e) => {
+        if (e.key === "Enter") {
+            addTodo();
+        }
+    }
+
+    const addDataInLocalstorage = () => {
+        if (todoList.length > 0) {
+            localStorage.setItem("todoList", JSON.stringify(todoList));
+        }
+    }
+
+
+    const fetchLocalstorageData = () => {
+        const todoListData = JSON.parse(localStorage.getItem("todoList"))
+        settodoList(todoListData)
+    }
+
+    // useEffect is react functional hook which is use for manage the sideeffects of react compontnents 
+    // for fetch api data 
+    // for call IoAlertSharp, loadings, dependacies 
+
+    useEffect(() => {
+        fetchLocalstorageData();
+    }, [])
+
+
+
+    useEffect(() => {
+        addDataInLocalstorage();
+    }, [todoList]);
+
+
+    const handleDeleteTask = (taskId) => {
+        const filteredTodoList = todoList.filter((task) => task.id !== taskId);
+        console.log(filteredTodoList, "filteredTodoList")
+        settodoList(filteredTodoList);
+    }
 
     return (
-        <>
+        <div className="container">
+            {console.log("inside UI")}
             <center>
                 <h1>TODO List</h1>
-                <input type="text" value={input} onChange={handleInputChange} />
-                <button onClick={addTodo}>Add</button>
-                <br />
-                <table>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Enter taskName" value={input} onChange={handleInputChange} onKeyDown={handleInputKeyDown} />
+                </div>
+
+                <button onClick={addTodo} className="btn btn-sm btn-primary">Add</button>
+                <hr />
+                <table className="table mt-4">
                     <thead>
                         <tr>
                             <th>#</th>
                             <th>Task Name</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
+                    <tbody>
+                        {todoList.length > 0 ?
+                            todoList.map((task) => {
+                                return (
+                                    <tr>
+                                        <td>{task.id}</td>
+                                        <td>{task.taskName}</td>
+                                        <td>
+                                            <button className="btn m-1 btn-sm btn-primary" ><MdEditSquare /></button>
+                                            <button className="btn m-1 btn-sm btn-danger" onClick={() => handleDeleteTask(task.id)}><MdDelete /></button>
+                                        </td>
+                                    </tr>
+                                )
+                            }) :
+                            <tr>
+                                <td><p>No data found</p></td>
+                            </tr>
+                        }
+                    </tbody>
                 </table>
             </center>
-        </>
+        </div>
     )
 }
 
