868c6f6b9be88472ac2852bbc30dd8703ea19bba
Shailesh Gokhale <68338746+shailesh-ss-19-11@users.noreply.github.com>
2025-10-15 21:01:51 +0530

    edit modal

--- src/todolist/Todo.jsx
+++ src/todolist/Todo.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
+import { Modal } from "react-bootstrap";
 import { MdDelete, MdEditSquare } from "react-icons/md";
 
 const Todo = () => {
-    console.log("inside component");
     const [input, setinput] = useState("");
     const [todoList, settodoList] = useState([]);
-
+    const [showModal, setshowModal] = useState(false);
+    const [task, settask] = useState({});
     const handleInputChange = (e) => {
         setinput(e.target.value);
     }
@@ -25,7 +26,6 @@ const Todo = () => {
         }
     }
 
-    console.log(todoList)
 
     const handleInputKeyDown = (e) => {
         if (e.key === "Enter") {
@@ -42,9 +42,9 @@ const Todo = () => {
 
     const fetchLocalstorageData = () => {
         const todoListData = JSON.parse(localStorage.getItem("todoList"))
-        if(todoListData?.length>0){
+        if (todoListData?.length > 0) {
             settodoList(todoListData)
-        }else{
+        } else {
             settodoList([])
         }
     }
@@ -66,13 +66,32 @@ const Todo = () => {
 
     const handleDeleteTask = (taskId) => {
         const filteredTodoList = todoList.filter((task) => task.id !== taskId);
-        console.log(filteredTodoList, "filteredTodoList")
         settodoList(filteredTodoList);
+    }
+
+    console.log(task);
+
+    const handleEditTask = (e) => {
+        if (e.target.value !== "") {
+            const obj = { ...task, taskName: e.target.value }
+            settask(obj);
+        }
+    }
+
+    const updateTask = () => {
+        const existingList = [...todoList];
+        console.log(existingList);
+        const findedElement = existingList.find((item) => item.id === task.id);
+        console.log(findedElement);
+        const index = existingList.indexOf(findedElement);
+        console.log(index);
+        existingList[index] = task;
+        settodoList(existingList);
+        setshowModal(false);
     }
 
     return (
         <div className="container">
-            {console.log("inside UI")}
             <center>
                 <h1>TODO List</h1>
                 <div className="input-group mb-3">
@@ -97,7 +116,7 @@ const Todo = () => {
                                         <td>{task.id}</td>
                                         <td>{task.taskName}</td>
                                         <td>
-                                            <button className="btn m-1 btn-sm btn-primary" ><MdEditSquare /></button>
+                                            <button className="btn m-1 btn-sm btn-primary" onClick={() => { settask(task); setshowModal(true) }}><MdEditSquare /></button>
                                             <button className="btn m-1 btn-sm btn-danger" onClick={() => handleDeleteTask(task.id)}><MdDelete /></button>
                                         </td>
                                     </tr>
@@ -110,6 +129,30 @@ const Todo = () => {
                     </tbody>
                 </table>
             </center>
+            {/* conditional rendering  */}
+            {/* {showModal ? <h1>showmodal</h1> : null}
+            {showModal ? <button onClick={() => { setshowModal(false) }}>close Modal</button> : null} */}
+            <Modal
+                onHide={() => setshowModal(false)}
+                show={showModal}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Edit Task
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="input-group mb-3">
+                        <input type="text" className="form-control" defaultValue={task?.taskName} onChange={handleEditTask} />
+                    </div>
+                    <div>
+                        <button className="btn btn-sm btn-primary" onClick={updateTask}>Submit</button>
+                    </div>
+                </Modal.Body>
+            </Modal>
         </div>
     )
 }
